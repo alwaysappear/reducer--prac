9ae5a25c9c3a03a78a7b1ec90e1268a7ac01208f
Raheem Kawojue <kawojue08@gmail.com>
2022-12-02 15:10:42 +0100

    OnChange done

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
 import { FaTrash } from 'react-icons/fa'
 
-const Item = ({ item, remove }) => {
-    const [done, setDone] = useState(false)
-    const { id, name } = item
+const Item = ({ item, remove, handleDone }) => {
+    const { id, name, done } = item
 
     return (
         <article className="item">
             <div>
-                <input type="checkbox"
-                    value={done} onChange={(e) => setDone(e.target.value)} />
+                <input type="checkbox" checked={done}
+                    onChange={() => handleDone(id)} />
                 <h3>{name}</h3>
             </div>
             <button className="btn-rm" onClick={() => remove(id)}>
