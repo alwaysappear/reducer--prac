d3469d6703f828d83e28de536d33f422e9be7ce6
Raheem Kawojue <kawojue08@gmail.com>
2022-12-02 13:44:50 +0100

    close modal func

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useReducer, useState, useRef } from 'react'
+import Item from './components/Item'
 import reducer from './components/reducer'
-import { FaTrash } from 'react-icons/fa'
+import { useReducer, useState, useRef } from 'react'
+import ModalContent from './components/ModalContent'
 
 const defaultStates = {
   items: [],
@@ -9,36 +10,38 @@ const defaultStates = {
 }
 
 function App() {
-  const [nameEl] = useRef(null)
+  const nameEl = useRef(null)
   const [name, setName] = useState('')
-  const [done, setDone] = useState(false)
   const [state, dispatch] = useReducer(reducer, defaultStates)
 
   const submit = (e) => {
     e.preventDefault()
+    const setID = new Date().getTime().toString()
+    const newItem = { id: setID, name }
+    if (name) {
+      dispatch({ type: 'ADD', payload: newItem })
+    }
+    setName('')
+    nameEl.current.focus()
+  }
+
+  const closeModal = () => {
+    dispatch({ type: 'CLOSE_MODAL' })
   }
 
   return (
     <main>
+      {state.modal && <ModalContent content={state.modalContent} close={closeModal} />}
       <form onSubmit={(e) => submit(e)}>
         <div>
-          <input type="text" ref={nameEl}
-            value={name} onChange={() => setName(e.target.value)} />
+          <input type="text" ref={nameEl} autoFocus
+            value={name} onChange={(e) => setName(e.target.value)} />
           <button className="btn-add">add</button>
         </div>
       </form>
       <section className="item-container">
         {state.items.map(item => (
-          <article className="item" key={item.id}>
-            <div>
-              <input type="checkbox" autoFocus
-                value={done} onChange={() => setDone(e.target.value)} />
-              <h3>{item.name}</h3>
-            </div>
-            <button className="btn-rm">
-              <FaTrash />
-            </button>
-          </ article>
+          <Item item={item} key={item.id} />
         ))}
       </section>
     </main>
