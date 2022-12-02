c3bf30c6237f2b3b13796e19e21ca418996dcc6f
Raheem Kawojue <kawojue08@gmail.com>
2022-12-02 14:48:07 +0100

    remove btn

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
 import { FaTrash } from 'react-icons/fa'
 
-const Item = (item) => {
+const Item = ({ item, remove }) => {
+    const [done, setDone] = useState(false)
+    const { id, name } = item
 
     return (
         <article className="item">
             <div>
-                {/* <input type="checkbox" autoFocus
-                    value={done} onChange={() => setDone(e.target.value)} /> */}
-                <h3>{item.name}</h3>
+                <input type="checkbox"
+                    value={done} onChange={(e) => setDone(e.target.value)} />
+                <h3>{name}</h3>
             </div>
-            <button className="btn-rm">
+            <button className="btn-rm" onClick={() => remove(id)}>
                 <FaTrash />
             </button>
         </ article>
