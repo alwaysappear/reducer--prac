e9c746a225d1ba7ff3ba3515c1b7c14c80f04c1c
Raheem Kawojue <kawojue08@gmail.com>
2022-12-02 14:47:05 +0100

    more colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,10 @@ module.exports = {
     extend: {
       colors: {
         'pry-clr-0': 'rgb(241, 245, 248)',
+        'pry-clr-1': 'rgb(30, 113, 221)',
+        'pry-clr-2': 'rgb(30, 89, 189)',
+        'pry-clr-3': 'rgb(16, 42, 66)',
+
       },
       fontFamily: {
         cabin: ['Cabin', 'sans-serif']
